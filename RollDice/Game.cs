602aa7f358f74ac92869c7cc2d552ee6759ef766
mikhita <mikheilispiriani@yahoo.com>
2024-02-08 20:34:18 +0400

    first commit

---
+++ RollDice/Game.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DiceRollGame
+{
+    class Game
+    {
+        private int randomNumber;
+        public void Start()
+        {
+            Random random = new Random();
+            randomNumber = random.Next(1,7);
+
+            Console.WriteLine("Dice rolled. Guess what number it shows in 3 tries.");
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Enter your guess (1-6): ");
+                int guess = int.Parse(Console.ReadLine());
+
+                if(guess == randomNumber)
+                {
+                    Console.WriteLine("You win");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong number");
+                }
+                
+            }
+
+            Console.WriteLine("You lose");
+            Console.ReadKey();
+        }
+    }
+}
